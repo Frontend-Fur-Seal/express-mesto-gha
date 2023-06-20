f2dedb41d3d512b83b39bd3092f7db0b65a57630
Vladimir Sherstnev <voralbergann@gmail.com>
2023-06-20 17:08:57 +0300

    add linter

--- app.js
+++ app.js
@@ -5,16 +5,14 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
 
 app.get('/', (req, res) => {
-    res.send(
-          `<html>
-          <body>
-              <p>Ответ на сигнал из далёкого космоса:</p>
-              <h1>Привет!</h1>
-          </body>
-          </html>`
-      );
-  });
+  res.send(`<html>
+      <body>
+        <p>Ответ на сигнал из далёкого космоса:</p>
+        <h1>Привет, человек!</h1>
+      </body>
+  </html>`);
+});
