235a0ba0675a6bd4bf26d58926a31b13816a993d
Vladimir Sherstnev <voralbergann@gmail.com>
2023-06-21 20:38:48 +0300

    commit

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.get('*', (req, res) => {
-  res.send('Incorrect', 404);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Incorrect' });
 });
 
 app.listen(PORT, () => {
