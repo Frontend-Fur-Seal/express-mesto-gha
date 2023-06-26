15e7790b47e191da359fb07212fd143a58e8e885
Vladimir Sherstnev <voralbergann@gmail.com>
2023-06-26 15:39:06 +0300

    login

--- models/user.js
+++ models/user.js
@@ -30,11 +30,12 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     require: true,
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
